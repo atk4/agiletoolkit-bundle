82c9141ba194c1d0907f542ed7ec7a1af4803ff3
alexeyostapets <oleksii.ostapets@gmail.com>
2013-11-25 17:13:49 +0200

    tests example

---
+++ tests/index.php
@@ -0,0 +1,5 @@
+<?php
+include '../vendor/atk4/atk4/loader.php';
+$api=new TestApi('test_project');
+$api->main();
+?>
---
+++ tests/page/model.php
@@ -0,0 +1,11 @@
+<?php
+
+class page_model extends Page_Tester {
+    public $proper_responses=array(
+        "Test_true"=>true,
+        );
+    function test_true(){
+        return true;
+    }
+}
+
