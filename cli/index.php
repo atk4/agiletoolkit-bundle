d7c2081120428650c58a2e5949b8819025351136
vadym <vadym@xxx.(none)>
2013-11-26 11:51:44 +0200

    cli fix and $argv

--- cli/index.php
+++ cli/index.php
@@ -3,4 +3,5 @@
 chdir('..');
 require'vendor/autoload.php';
 $api=new Cli('cli');
+$api->setArgv($argv);
 $api->main();
--- cli/lib/Cli.php
+++ cli/lib/Cli.php
@@ -1,9 +1,18 @@
 <?php
 class Cli extends ApiCLI {
+    public $argv;
+    function _beforeInit() {
+        $this->pm = $this->add('Controller_PageManager');
+        parent::_beforeInit();
+    }
     function init() {
         parent::init();
     }
+    // console script args
+    function setArgv($argv) {
+        $this->argv = $argv;
+    }
     function main(){
-        echo 'test apiCli';
+        var_dump($this->argv);
     }
 }
