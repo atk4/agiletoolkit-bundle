4f856d7d6acae4d290e0f396ddbc66b194ec3fb5
vadym <vadym@xxx.(none)>
2013-12-02 14:59:52 +0200

    fix

--- app/test/public/index.php
+++ app/test/public/index.php
@@ -1,6 +1,7 @@
 <?php
-chdir('../../..');
-require'vendor/autoload.php';
+chdir('..');
+require_once '../../vendor/autoload.php';
+require_once 'lib/TestApi.php';
 $api=new TestApi('test');
 $api->main();
 
