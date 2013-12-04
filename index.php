5208364fb808011143fb5911414df09be0022c9e
Romans Malinovskis <r@agiletech.ie>
2013-12-04 00:32:54 +0000

    add installer and make it work

--- index.php
+++ index.php
@@ -6,3 +6,6 @@ if(file_exists('config.php')){
 
 include_once'vendor/atk4/atk4/loader.php';
 include_once'atk4-ide/init.php';
+include_once'atk4-ide/api/AgileToolkit/Installer.php';
+$api=new AgileToolkit_Installer('new_atk4_install');
+$api->main();
