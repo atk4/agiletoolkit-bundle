4877bca3584403bbd8264e59614a4c1ed35db633
vadym <vadym@xxx.(none)>
2013-12-05 14:54:08 +0200

    qwe

--- index.php
+++ index.php
@@ -6,4 +6,21 @@
  * Time: 12:01 PM
  * To change this template use File | Settings | File Templates.
  */
-echo 'this is index';
+
+//chdir('atk4-ide');
+
+require_once './vendor/autoload.php';
+require_once './atk4-ide/api/AgileToolkit/Installer.php';
+
+if (file_exists('./atk4-ide.phar')) {
+  include_once './atk4-ide.phar';
+} elseif (file_exists('./atk4-ide') && is_dir('./atk4-ide')){
+  include_once './atk4-ide/init.php';
+} else {
+    exit('Download atk4-ide.phar to use installer.');
+}
+
+
+
+$api = new AgileToolkit_Installer('atl4-installer');
+$api->main();
