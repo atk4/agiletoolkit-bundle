3c12e38adaa4f73c119294bd6ff98ab182db7b9d
vadym <vadym@xxx.(none)>
2013-12-09 20:28:18 +0200

    config-auto

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ if (file_exists('./atk4-ide.phar')) {
 
 include_once'vendor/atk4/atk4/loader.php';
 include_once'atk4-ide/init.php';
+include_once'atk4-ide/api/Controller/Config.php';
 include_once'atk4-ide/api/AgileToolkit/Installer.php';
 $api=new AgileToolkit_Installer('new_atk4_install');
 $api->main();
