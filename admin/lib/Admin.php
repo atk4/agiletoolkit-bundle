f9eb0c92c395e8c6af0a06ca411e50467b108609
vadym <vadym@xxx.(none)>
2014-01-30 14:35:40 +0200

    sandbox logic moved to sandbox

--- admin/lib/Admin.php
+++ admin/lib/Admin.php
@@ -17,35 +17,4 @@ class Admin extends Api_Admin {
             $this->sandbox->police->addDebugView($this->api->layout);
         }
     }
-
-    function addAddonsLocations() {
-        $a = $this->add('sandbox\\Controller_InstallAddon');
-        $base_path = $this->pathfinder->base_location->getPath();
-        foreach ($a->getSndBoxAddonReader()->getReflections() as $addon) {
-            // Private location contains templates and php files YOU develop yourself
-            /*$this->private_location = */
-            $this->api->pathfinder->addLocation(array(
-                'docs'      => 'docs',
-                'php'       => 'lib',
-                'addons'    => '../..',
-                'page'      => 'page',
-                'template'  => 'templates',
-            ))
-                    ->setBasePath($base_path.'/../'.$addon->get('addon_full_path'))
-            ;
-
-            $addon_public = $addon->get('addon_symlink_name');
-            // this public location cotains YOUR js, css and images, but not templates
-            /*$this->public_location = */
-            $this->api->pathfinder->addLocation(array(
-                'js'     => 'js',
-                'css'    => 'css',
-                'public' => './',
-                //'public'=>'.',  // use with < ?public? > tag in your template
-            ))
-                    ->setBasePath($base_path.'/'.$addon->get('addon_public_symlink'))
-                    ->setBaseURL($this->api->url('/').$addon_public) // $this->api->pm->base_path
-            ;
-        }
-    }
 }
