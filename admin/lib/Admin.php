c7a5c004cd7c947c52505212108f5dc5bcfe2cbc
Mayur Ahir <ahirmayur@gmail.com>
2014-05-13 11:49:33 +0100

    Police alert

--- admin/lib/Admin.php
+++ admin/lib/Admin.php
@@ -1,13 +1,15 @@
 <?php
+
 class Admin extends App_Admin {
 
     function init() {
         parent::init();
 
-        $this->api->pathfinder->addLocation(array(
-            'addons'    => array('addons','vendor'),
-        ))
-                ->setBasePath($this->pathfinder->base_location->getPath().'/..')
+        $this->api->pathfinder
+            ->addLocation(array(
+                'addons' => array('addons', 'vendor'),
+            ))
+            ->setBasePath($this->pathfinder->base_location->getPath() . '/..')
         ;
 
         $this->api->menu->addMenuItem('/', 'Home');
