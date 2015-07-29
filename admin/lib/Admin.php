7b2daffb9ec9b40e29b61b1c0c801111df9257b0
Romans Malinovskis <romans@agiletoolkit.org>
2015-07-29 11:28:33 +0300

    some cleanup

--- admin/lib/Admin.php
+++ admin/lib/Admin.php
@@ -5,13 +5,6 @@ class Admin extends App_Admin {
     function init() {
         parent::init();
 
-        $this->api->pathfinder
-            ->addLocation(array(
-                'addons' => array('addons', 'vendor'),
-            ))
-            ->setBasePath($this->pathfinder->base_location->getPath() . '/..')
-        ;
-
-        $this->api->menu->addMenuItem('/', 'Home');
+        $this->api->menu->addItem('Dashboard', '/');
     }
 }
