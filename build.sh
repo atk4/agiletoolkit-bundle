899d20807be54c107225c611b6d5e4968e88e600
Romans Malinovskis <romans@agiletoolkit.org>
2017-08-07 14:49:23 +0100

    update build instructions

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
-# Read versio
-version=`cat vendor/atk4/atk4/VERSION`
+version=`cat composer.lock | jq '.packages[] | select (.name == "atk4/ui").version' -r `
+
+composer update
 
 # Clean up
 rm -rf dist/agiletoolkit
@@ -11,104 +12,32 @@ mkdir -p dist/agiletoolkit
 # Copy some stuff inside
 cp -aR vendor dist/agiletoolkit/
 
-cp -aR admin dist/agiletoolkit/
-cp -aR frontend dist/agiletoolkit/
-#cp -aR shared dist/agiletoolkit/
-#cp -a config-auto.php dist/agiletoolkit/
-cp -a config-default.php dist/agiletoolkit/
-#cp -aR atk4-ide/public dist/agiletoolkit/atk4-ide/
+cp -a admin.php dist/agiletoolkit/
 cp -a index.php dist/agiletoolkit/
-cp -a run.* dist/agiletoolkit/
 
-# Next - remove .git folders to conserve space, not sure if it will kill composer
-( cd dist/agiletoolkit/ && rm -rf .git )
+# Launch apps
+cp -aR Agile\ Toolkit.app dist/agiletoolkit/
+cp -a run.sh dist/agiletoolkit/
+cp -a run.bat dist/agiletoolkit/
 
-# create PHAR shell
-rm -rf dist/tmp
-mkdir -p dist/tmp/src
-mkdir -p dist/tmp/src/cert
-
-
-
-#cp -aR agiletoolkit-sandbox/phar_skel/* dist/tmp
-cp -aR agiletoolkit-sandbox/{lib,addons,template,init.php} dist/tmp/src
-
-
-# Grab fresh certificate from agiletoolkit.org site
-echo -n | openssl s_client -connect agiletoolkit.org:443  | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'  > dist/tmp/src/cert/agiletoolkit.cert
-
-
-{
-  cat vendor/atk4/atk4/VERSION
-  echo -n "Branch: "
-  (cd vendor/atk4/atk4/; git rev-parse --abbrev-ref HEAD)
-  echo "Build: `date '+%Y-%m-%d'`"
-
-  echo -n "Sandbox Revision: "
-  (cd agiletoolkit-sandbox/; git rev-list --count HEAD)
-
-  echo -n "ATK4 Revision: "
-  (cd vendor/atk4/atk4/; git rev-list --count HEAD)
-
-  ver=`(cd agiletoolkit-css; cat framework/less/agiletoolkit.less | head -n10 | grep 'Agile Toolkit' | sed 's/.* v//')`
-  rev=`(cd agiletoolkit-css; git rev-list --count HEAD)`
-  echo "Agile CSS: $ver ($rev)"
-
-} >> dist/agiletoolkit/VERSION
-
-{
-  cat agiletoolkit-sandbox/addons/sandbox/lib/Initiator.php | grep -A1 getVersion | tail -1 | cut -d"'" -f2
-} >> dist/agiletoolkit/VERSION.Sandbox
-
-cp dist/agiletoolkit/VERSION dist/tmp/src/VERSION
-cp dist/agiletoolkit/VERSION.Sandbox dist/tmp/src/VERSION.Sandbox
-
-( cd phar-packer/; php create-phar.php )
-#( cd _build/atk4_phar; php create-phar.php )
-
-#cp dist/tmp/agiletoolkit-sandbox.phar dist/agiletoolkit/
-bundle_version=`cat agiletoolkit-sandbox/VERSION`
-mkdir /www/agiletoolkit.org/public/dist/$bundle_version
-cp dist/tmp/agiletoolkit-sandbox.phar /www/agiletoolkit.org/public/dist/$bundle_version/
-#cp _build/atk4_phar/build/atk4-ide.phar dist/agiletoolkit/
-
-cp gitignore-distrib dist/agiletoolkit/.gitignore
-cp composer.json dist/agiletoolkit/
-cp composer.lock dist/agiletoolkit/
-rm dist/agiletoolkit/vendor/atk4/atk4/.htaccess
-
-# Todo - we should compile it here instead
-cp -aR vendor/atk4/atk4/public/atk4 dist/agiletoolkit/admin/public/
-cp -aR vendor/atk4/atk4/public/atk4 dist/agiletoolkit/frontend/public/
-
-# Strip group write permssions as it makes people upset
-( cd dist; chmod g-w -R agiletoolkit )
-
-echo "cleaning .git from dist"
-
-( cd dist && find -name .git | while read x; do rm -r $x; done )
+cp -a .gitignore dist/agiletoolkit/
 
 # tar, but make sure
-( cd dist; tar --no-same-owner --no-xattrs -czf agiletoolkit-${version}.tgz \
-  agiletoolkit/ && cp agiletoolkit-${version}.tgz /www/agiletoolkit.org/public/dist/ && \
-  cp agiletoolkit/agiletoolkit-sandbox.phar /www/agiletoolkit.org/public/dist/ )
+( cd dist; tar --no-same-owner -czf agiletoolkit-${version}.tgz \
+    agiletoolkit/ && rm -r agiletoolkit )
 
-cat dist/agiletoolkit/VERSION
-
-rm -rf dist
+#cp dist/agiletoolkit-${version}.tgz /www/agiletoolkit.org/public/dist/
 
 # next , let's upload file to the server
 
-rm -rf /www/install-test/agiletoolkit/
-(
-  cd /www/install-test/;
-  tar -zxf /www/agiletoolkit.org/public/dist/agiletoolkit-${version}.tgz
-  sudo chgrp upload -R .
-)
+#rm -rf /www/install-test/agiletoolkit/
+#(
+#  cd /www/install-test/;
+#  tar -zxf /www/agiletoolkit.org/public/dist/agiletoolkit-${version}.tgz
+#  sudo chgrp upload -R .
+#)
 
 # give rights so others can do it too
-chmod -R g+w /www/install-test/agiletoolkit/
+#chmod -R g+w /www/install-test/agiletoolkit/
 
-echo "Installed. Archive is http://www4.agiletoolkit.org/dist/agiletoolkit-${version}.tgz "
-echo "Sandbox is http://www4.agiletoolkit.org/dist/$bundle_version/agiletoolkit-sandbox.phar "
-echo "Test: http://install-test-399482.agiletoolkit.org/agiletoolkit/"
+echo "Ready! dist/agiletoolkit-${version}.tgz (`du -h dist/agiletoolkit-${version}.tgz | awk '{print $1}'`)"
