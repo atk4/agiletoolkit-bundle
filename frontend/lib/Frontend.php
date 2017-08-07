05410ab914c321963c08a352766cc7fcc9cc4ef8
Romans Malinovskis <romans@agiletoolkit.org>
2017-08-07 14:21:43 +0100

    add Mac Web App launcher

--- frontend/lib/Frontend.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Frontend extends ApiFrontend {
-    function init() {
-        parent::init();
-
-        $this->add('Layout_Centered');
-        // you can also use Layout_Fluid
-    }
-}
